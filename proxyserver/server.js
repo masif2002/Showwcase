00f2c670c01ecd3cbd98cc36555eb72ae9a311a9
darrshantg <114505879+darrshantg@users.noreply.github.com>
2023-10-24 10:09:27 +0530

    Create server.js

---
+++ proxyserver/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = (...args) => 
+    import ('node-fetch').then(({default: fetch}) => fetch(...args));
+const bodyparser = require('body-parser');
+
+const CLIENT_ID = "9ca15689cc4006dee638";
+
+const app = express();
+app.use(cors());
+app.use(bodyparser.json());
+
+app.get('/getAccessToken', async (req,res) => {
+    const params = "?client_id=" + CLIENT_ID + "&client_secret=" + process.env.CLIENT_SECRET + "&code=" + req.query.code;
+
+    await fetch("http://github.com/login/oauth/access_token" + params, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        res.json(data);
+    })
+})
+
+app.get('/getUserData', async (req, res) => {
+    console.log(req.get('Authorization'));
+    
+    await fetch("https://api.github.com/user", {
+        method: "GET",
+        headers: {
+            "Authorization": req.get("Authorization")
+        }}).then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+            res.json(data);
+        })
+})
+
+app.listen(4000, () => {
+    console.log("Server running on port 4000");
+})
