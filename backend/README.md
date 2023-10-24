d768a7559b90df236bf9d315cccac1e59563c867
masif2002 <masif2002@outlook.com>
2023-10-24 11:49:24 +0530

    Add backend

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+## Installation 
+* Install `venv` from python
+* Create a virtual environment
+* Install dependencies from `requirements.txt`
+
+## Start Server
+* Activate virtual environment
+* Run `startServer.sh`
---
+++ backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, flash, jsonify
+import subprocess
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+@app.route("/deploy", methods=["GET", "POST"])
+def deploy():
+
+  # Extract Project URL
+  project_url = request.json['project']
+
+  # Launch the container
+  p = subprocess.Popen(["../core/initiateDeployment.sh", project_url]) 
+  code = p.wait()
+
+  print(code)
+
+  
+  return jsonify({
+    "url": "ws://localhost:6080", 
+    "vncpassword": "password"
+  }) 
